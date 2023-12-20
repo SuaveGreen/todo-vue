1638a1d3bcdbbd0c9e8380663aae203a6cae21e0
Pedro Caetano <pedro.kaetano2003@gmail.com>
2023-12-20 00:00:50 -0300

    componitizando a aplicação

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,12 @@
+<script setup>
+    const props = defineProps(['tarefasPendestes'])
+</script>
+
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+        <h1>Minhas Tarefas</h1>
+        <p>
+            Você possui {{ props.tarefasPendestes }} tarefas pendentes
+        </p>
+    </header>
+</template>
